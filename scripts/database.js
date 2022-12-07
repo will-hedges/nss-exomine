05cde096c9139d1450a4c4446bb4a75a18e0c11c
Logan Gray <loganallengray@gmail.com>
2022-12-07 15:26:34 -0600

    added facilityInventories property to database

--- scripts/database.js
+++ scripts/database.js
@@ -1,4 +1,15 @@
 const database = {
+    facilityInventories: [
+        {id: 1, mineralId: 1, amount: 12, facilityId: 1},
+        {id: 2, mineralId: 4, amount: 17, facilityId: 1},
+        {id: 3, mineralId: 5, amount: 3, facilityId: 1},
+        {id: 4, mineralId: 1, amount: 16, facilityId: 2},
+        {id: 5, mineralId: 3, amount: 10, facilityId: 2},
+        {id: 6, mineralId: 2, amount: 5, facilityId: 2},
+        {id: 7, mineralId: 3, amount: 28, facilityId: 3},
+        {id: 8, mineralId: 5, amount: 8, facilityId: 3},
+        {id: 9, mineralId: 2, amount: 15, facilityId: 3},
+    ],
     minerals: [
         {id: 1, type: "Iron"},
         {id: 2, type: "Chromium"},
