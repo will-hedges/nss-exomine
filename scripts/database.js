0f33ae092a06f381239b80d9b839a090bb8cac4f
Jeremy Goodman <jeremygoodman52@gmail.com>
2022-12-07 14:50:49 -0600

    added governors

--- scripts/database.js
+++ scripts/database.js
@@ -1,20 +1,52 @@
 const database = {
-    transientState: {}
+    transientState: {},
+    governors:[
+    {
+        id: 1,
+        name: "George Simmonds",
+        actiive: true,
+        colonyId: 4
+    }, 
+    {
+        id: 2,
+        name: "Lisa Hartford",
+        actiive: true,
+        colonyId: 5
+    },
+    {
+        id: 3,
+        name: "Yoko Ono",
+        actiive: true,
+        colonyId: 1
+    },
+    {
+        id: 4,
+        name: "Mike Hunt",
+        actiive: true,
+        colonyId: 2
+    },
+    {
+        id: 5,
+        name: "Jack Dorsey",
+        actiive: true,
+        colonyId: 3
+    },
+    ]
 }
 
 export const setFacility = (facilityId) => {
     database.transientState.selectedFacility = facilityId
-    document.dispatchEvent( new CustomEvent("stateChanged") )
+    document.dispatchEvent(new CustomEvent("stateChanged"))
 }
 
 export const getFacilities = () => {
-    return database.facilities.map(f => ({...f}))
+    return database.facilities.map(f => ({ ...f }))
 }
 
 export const purchaseMineral = () => {
 
-        // Broadcast custom event to entire documement so that the
-        // application can re-render and update state
-        document.dispatchEvent( new CustomEvent("stateChanged") )
-    }
+    // Broadcast custom event to entire documement so that the
+    // application can re-render and update state
+    document.dispatchEvent(new CustomEvent("stateChanged"))
+}
 }
