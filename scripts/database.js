5571745e83228b3fb778056ffd155c6b628b2925
Will Hedges <wshedges@outlook.com>
2022-12-07 14:49:37 -0600

    added colonies

--- scripts/database.js
+++ scripts/database.js
@@ -1,20 +1,40 @@
 const database = {
-    transientState: {}
-}
+  colonies: [
+    {
+      id: 1,
+      name: "Earth",
+    },
+    {
+      id: 2,
+      name: "Mars",
+    },
+    {
+      id: 3,
+      name: "Europa",
+    },
+    {
+      id: 4,
+      name: "Titan",
+    },
+    {
+      id: 5,
+      name: "Proteus",
+    },
+  ],
+  transientState: {},
+};
 
 export const setFacility = (facilityId) => {
-    database.transientState.selectedFacility = facilityId
-    document.dispatchEvent( new CustomEvent("stateChanged") )
-}
+  database.transientState.selectedFacility = facilityId;
+  document.dispatchEvent(new CustomEvent("stateChanged"));
+};
 
 export const getFacilities = () => {
-    return database.facilities.map(f => ({...f}))
-}
+  return database.facilities.map((f) => ({ ...f }));
+};
 
 export const purchaseMineral = () => {
-
-        // Broadcast custom event to entire documement so that the
-        // application can re-render and update state
-        document.dispatchEvent( new CustomEvent("stateChanged") )
-    }
-}
+  // Broadcast custom event to entire documement so that the
+  // application can re-render and update state
+  document.dispatchEvent(new CustomEvent("stateChanged"));
+};
