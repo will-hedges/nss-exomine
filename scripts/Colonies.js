1b6fd8d21f3cc3ac84dbaa32c07cec9d976e0f89
Will Hedges <wshedges@outlook.com>
2022-12-08 11:53:26 -0600

    added colonies dropdown * capitalized Exomine.js module

--- scripts/Colonies.js
+++ scripts/Colonies.js
@@ -1,8 +1,19 @@
 import { getColonies } from "./database.js";
 
+const colonies = getColonies();
+
 export const Colonies = () => {
     return `
-        <h2>Colonies</h2>
-        ${getColonies()}
+        <section class="choices__colonies">
+            <h2>Colonies</h2>
+            <select id="colony">
+                <option value="0">Select a colony</option>
+                ${colonies
+                    .map((colony) => {
+                        return `<option value="${colony.id}">${colony.name}</option>`;
+                    })
+                    .join("")}
+            </select>
+        </section>
     `;
 };
