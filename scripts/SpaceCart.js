90a9f59350e29095a9cbe5942f85868984de7b82
Will Hedges <wshedges@outlook.com>
2022-12-08 15:32:21 -0600

    placed space cart on page

---
+++ scripts/SpaceCart.js
@@ -0,0 +1,7 @@
+export const SpaceCart = () => {
+    return `
+        <h2>Space Cart</h2>
+        <div id="space-cart"></div>
+        <button type="button" id="purchase-button">Purchase Mineral</button>
+    `;
+};
