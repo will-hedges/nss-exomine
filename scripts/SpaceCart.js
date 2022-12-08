918d1ca94ba52147e76187748cafb4eb27f15897
Will Hedges <wshedges@outlook.com>
2022-12-08 15:37:45 -0600

    wrapped space cart in a container div

--- scripts/SpaceCart.js
+++ scripts/SpaceCart.js
@@ -1,7 +1,9 @@
 export const SpaceCart = () => {
     return `
-        <h2>Space Cart</h2>
-        <div id="space-cart"></div>
-        <button type="button" id="purchase-button">Purchase Mineral</button>
+        <div id="space-cart__container">
+            <h2>Space Cart</h2>
+            <div id="space-cart"></div>
+            <button type="button" id="purchase-button">Purchase Mineral</button>
+        </div>
     `;
 };
