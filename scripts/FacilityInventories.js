977f409fce0b399f2a87e53ca5d7cf5b54f2282a
Logan Gray <loganallengray@gmail.com>
2022-12-09 11:54:28 -0600

    How about no

--- scripts/FacilityInventories.js
+++ scripts/FacilityInventories.js
@@ -1,30 +1,46 @@
-import { getFacilityInventories, getFacilities, getTransientData } from "./database.js";
+import { getFacilityInventories, getTransientData, getMinerals } from "./database.js";
 
-const facilities = getFacilities();
 const facilityInventories = getFacilityInventories();
+const minerals = getMinerals();
 
 /* 
     code should display the minerals available at a specific facility.
 
-    add event listener for state change custom event
-    check the transitory data for a facility id.
-    if its 0 or null or undefined, dont do anything.
-    if not, iterate through the facility inventories and check the facility id 
-    with a .find against the one in the transitory data.
-    send that to a function that displays the HTML
-    return a .map that displays all of the facility's minerals, and all the appropriate data
+    add event listener for state changed.
+    start HTML variable.
+    check if the transient data has a selected facility value higher than 0.
+    if true, iterate through facility inventories and grab the ones with the correct value.
+    iterate again through the available inventories, and match them up with the correct mineral types.
+    take the appopriate data and format it into html, add to HTML variable.
+    use function to send HTML to exomine.js
+
 */
 
-document.addEventListener("stateChanged", e => {
-    const transientData = getTransientData();
+export const FacilityInventories = (htmlList) => {
+    let html = "<h2>Facility Minerals</h2>\n";
 
-    if (transientData.facilityID > 0) {
-        const availableInventories = facilityInventories.find(inv => (inv.id === transientData.invID));
-
-        facilityInventories()
-    }
-})
-
-export const FacilityInventories = () => {
-    
+    return html += htmlList;
 };
+
+document.addEventListener("facilityChanged", e => {
+    const transientData = getTransientData();
+    let html = `<ul class="facilityInventories">`;
+
+    if (transientData.selectedFacility > 0) {
+        const availableInventories = facilityInventories.filter(inv => inv.facilityId === transientData.selectedFacility);
+
+        html += availableInventories.map(inv => {
+            for (const mineral of minerals) {
+                if (inv.mineralId === mineral.id) {
+                    return `<li>
+                        <input type="radio" name="facilityInventory" value="${inv.id}" />${inv.amount} tons of ${mineral.name}
+                    </li>`
+                }
+            }
+        })
+    }
+
+    html += "</ul>"
+
+    FacilityInventories(html);
+})
--- scripts/database.js
+++ scripts/database.js
@@ -57,7 +57,7 @@ export const getFacilities = () => {
 
 export const setFacility = (facilityId) => {
     database.transientState.selectedFacility = facilityId;
-    document.dispatchEvent(new CustomEvent("stateChanged"));
+    document.dispatchEvent(new CustomEvent("facilityChanged"));
 };
 
 export const getGovernors = () => {
