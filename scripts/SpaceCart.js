21f7c5a808f00ecc382c235d195431df14153c05
Logan Gray <loganallengray@gmail.com>
2022-12-16 14:39:17 -0600

    added a bunch of fancy css

--- scripts/SpaceCart.js
+++ scripts/SpaceCart.js
@@ -30,11 +30,9 @@ document.addEventListener("click", (e) => {
 
 export const SpaceCart = () => {
     return `
-        <div id="space-cart__container">
-            <h2>Space Cart</h2>
-            <div id="space-cart"></div>
-            <button type="button" id="purchase-button">Purchase Mineral</button>
-        </div>
+        <h2>Space Cart</h2>
+        <div id="space-cart"></div>
+        <button type="button" id="purchase-button">Purchase Mineral</button>
     `;
 };
 
